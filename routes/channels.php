d43dd6152883ec16df5e99222b3105bc0d3197c1
Alexander <kholyavskij@mail.ru>
2023-10-27 17:30:47 +0300

    .

--- routes/channels.php
+++ routes/channels.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('public', function () {
+    return true;
 });
 
 Broadcast::channel('chat', ChatChannel::class);
