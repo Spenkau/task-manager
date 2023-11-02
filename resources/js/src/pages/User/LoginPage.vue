6f34622cf2a6590a95bd80fc227d904f209afda1
ADMIN <bbadboy1337@gmail.com>
2023-11-02 15:27:40 +0300

    add login form

--- resources/js/src/pages/User/LoginPage.vue
+++ resources/js/src/pages/User/LoginPage.vue
@@ -1,5 +1,9 @@
 <template>
+<div class="login-page">
+    <v-form>
 
+    </v-form>
+</div>
 </template>
 
 <script>
@@ -8,6 +12,15 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "../../../../css/general";
 
+.login-page{
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-items: center;
+    align-items: center;
+}
 </style>
